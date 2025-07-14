459229adc01a5ff25b01153c408c7c3f840c368b
SURAJ045 <surajmp045@gmail.com>
2025-07-15 01:06:55 +0530

    added 404 file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Greeting from './components/Greeting';
 import WaterTracker from './components/WaterTracker';
 import MoodLogger from './components/MoodLogger';
